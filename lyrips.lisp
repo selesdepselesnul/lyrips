499b57dfeb8e5492f6da6a6534e7d482401ccd31
selesdepselesnul <selesdepselesnul@gmail.com>
2017-11-13 18:30:11 +0700

    change url structure

--- lyrips.lisp
+++ lyrips.lisp
@@ -6,11 +6,10 @@
   (let* ((->dash (lambda (x)  (values (cl-ppcre:regex-replace-all "\\s" x "-"))))
          (artist-metro (funcall ->dash artist))
          (song-metro (funcall ->dash song)))
-    (format nil "http://www.metrolyrics.com/~a-~a.html" song-metro artist-metro)))
+    (format nil "http://www.metrolyrics.com/~a-lyrics-~a.html" song-metro artist-metro)))
 
 (defun get-metrolyric (artist song)
-  (let* ((request (artist&song->metrolyrics-url artist song))
+  (let* ((request (dex:get (artist&song->metrolyrics-url artist song)))
          (parsed-content (plump:parse request))
-         (lyric-vec (lquery:$  parsed-content "p.verse" (text)))))
-  (reduce (lambda (x acc)  (concatenate 'string x acc))  lyric-vec))
-
+         (lyric-vec (lquery:$  parsed-content "p.verse" (text))))
+    (reduce (lambda (x acc)  (concatenate 'string x acc))  lyric-vec)))
