eb074a267a7cd5e7b0c5186b3ec1ae541ff53a0d
selesdepselesnul <selesdepselesnul@gmail.com>
2017-11-14 16:30:52 +0700

    add genius

--- lyrips.lisp
+++ lyrips.lisp
@@ -1,12 +1,14 @@
 ;;;; lyrips.lisp
 
 (in-package #:lyrips) 
-;; (ql:quickload '(:dexador :plump :lquery :lparallel))
+;; (ql:quickload '(:dexador :plump :lquery :lparallel :cl-ppcre))
+
+(defun space->- (x)
+  (values (cl-ppcre:regex-replace-all "\\s" x "-")))
 
 (defun create-metrolyrics-url (artist song)
-  (let* ((->dash (lambda (x)  (values (cl-ppcre:regex-replace-all "\\s" x "-"))))
-         (artist-metro (funcall ->dash artist))
-         (song-metro (funcall ->dash song)))
+  (let ((artist-metro (space->- artist))
+        (song-metro (space->- song)))
     (format nil "http://www.metrolyrics.com/~a-lyrics-~a.html" song-metro artist-metro)))
 
 (defun get-metrolyric (artist song)
@@ -34,3 +36,26 @@
         "(?s).+Usage of azlyrics\.com .+ Sorry about that\."
         (elt lyric-vec 3)"")"")""))))
 
+;; (with-open-file (str file-path
+;;                      :direction :output
+;;                      :if-exists :supersede
+;;                      :if-does-not-exist :create)
+;;   (format str large-string))
+
+(defun create-genius-url (artist song)
+  (let* ((artist-genius (space->- artist))
+         (song-genius (space->- song)))
+    (format nil "https://genius.com/~a-~a-lyrics" artist-genius song-genius)))
+
+(defun get-genius (artist song)
+  (let* ((request (dex:get (create-genius-url artist song)))
+         (parsed-content (plump:parse request))
+         (lyric-vec (lquery:$  parsed-content "div.lyrics" (text)))
+         (lyric (reduce (lambda (x acc)  (concatenate 'string x acc))  lyric-vec))) 
+    (values
+     (cl-ppcre:regex-replace "(?s).*Verse 1"
+                             (cl-ppcre:regex-replace
+                              "(?s)/sse.*/sse"
+                              lyric
+                              "")
+                             "[Verse 1"))))
