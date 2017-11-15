01170e0c606a88f0cc1e7e26e8b7a18694da9bcb
selesdepselesnul <selesdepselesnul@gmail.com>
2017-11-15 10:14:01 +0700

    refactor

--- lyrips.lisp
+++ lyrips.lisp
@@ -4,40 +4,62 @@
 ;; (ql:quickload '(:dexador :plump :lquery :lparallel :cl-ppcre :cl-strings))
 
 (defun space-> (str replacer)
-  (values (cl-ppcre:regex-replace-all "\\s" str replacer)))y
+  (values (cl-ppcre:regex-replace-all "\\s" str replacer)))
 
 (defun space->- (x)
   (values (cl-ppcre:regex-replace-all "\\s" x "-")))
 
-(defun create-metrolyrics-url (artist song)
+(defun dex-get (artist song f-url-creator)
+  (dex:get (funcall f-url-creator artist song)))
+
+(defun get-lyric (html-string selector f-html-lyric-mapper)
+  (let* ((parsed-content (plump:parse html-string))
+         (lyric-vec (lquery:$  parsed-content selector (text))))
+    (funcall f-html-lyric-mapper lyric-vec)))
+
+(defun get-lyric-with-dex (artist song f-url-creator selector f-html-lyric-mapper)
+  (let ((html-string (dex-get artist
+                              song
+                              f-url-creator)))
+    (get-lyric html-string selector f-html-lyric-mapper)))
+
+(defun metrolyrics-url-creator (artist song)
   (let ((artist-metro (space->- artist))
         (song-metro (space->- song)))
     (format nil "http://www.metrolyrics.com/~a-lyrics-~a.html" song-metro artist-metro)))
 
-(defun get-metrolyric (artist song)
-  (let* ((request (dex:get (create-metrolyrics-url artist song)))
-         (parsed-content (plump:parse request))
-         (lyric-vec (lquery:$  parsed-content "p.verse" (text))))
-    (reduce (lambda (x acc)  (concatenate 'string x acc))  lyric-vec)))
+(defun metrolyrics-mapper (xs)
+  (reduce (lambda (x acc)  (concatenate 'string x acc)) xs))
 
-(defun create-az-lyrics-url (artist song)
+(defun get-metrolyrics (artist song)
+  (get-lyric-with-dex artist
+                      song
+                      #'metrolyrics-url-creator
+                      "p.verse"
+                      #'metrolyrics-mapper))
+
+(defun azlyrics-url-creator (artist song)
   (let* ((->trim-all (lambda (x) (cl-ppcre:regex-replace-all " +" x "")))
          (artist-az (funcall ->trim-all artist))
          (song-az (funcall ->trim-all song)))
     (format nil "https://www.azlyrics.com/lyrics/~a/~a.html" artist-az song-az)))
 
-(defun get-azlyric (artist song)
-  (let* ((request (dex:get (create-az-lyrics-url artist song)))
-         (parsed-content (plump:parse request))
-         (lyric-vec (lquery:$  parsed-content "div.text-center" (text))))
-    (values
-     (cl-ppcre:regex-replace-all
-      ".*ba.*"
-      (cl-ppcre:regex-replace
-       "(?s)if  \\(.*"
-       (cl-ppcre:regex-replace
-        "(?s).+Usage of azlyrics\.com .+ Sorry about that\."
-        (elt lyric-vec 3)"")"")""))))
+(defun azlyrics-mapper (xs)
+  (values
+   (cl-ppcre:regex-replace-all
+    ".*ba.*"
+    (cl-ppcre:regex-replace
+     "(?s)if  \\(.*"
+     (cl-ppcre:regex-replace
+      "(?s).+Usage of azlyrics\.com .+ Sorry about that\."
+      (elt xs 3)"")"")"")))
+
+(defun get-azlyrics (artist song)
+  (get-lyric-with-dex artist
+                      song
+                      #'azlyrics-url-creator
+                      "div.text-center" 
+                      #'azlyrics-mapper))
 
 ;; (with-open-file (str file-path
 ;;                      :direction :output
@@ -45,25 +67,28 @@
 ;;                      :if-does-not-exist :create)
 ;;   (format str large-string))
 
-(defun create-genius-url (artist song)
+(defun genius-url-creator (artist song)
   (let* ((artist-genius (space->- artist))
          (song-genius (space->- song)))
     (format nil "https://genius.com/~a-~a-lyrics" artist-genius song-genius)))
 
-(defun get-genius (artist song)
-  (let* ((request (dex:get (create-genius-url artist song)))
-         (parsed-content (plump:parse request))
-         (lyric-vec (lquery:$  parsed-content "div.lyrics" (text)))
-         (lyric (reduce (lambda (x acc)  (concatenate 'string x acc))  lyric-vec))) 
-    (values
-     (cl-ppcre:regex-replace "(?s).*Verse 1"
-                             (cl-ppcre:regex-replace
-                              "(?s)/sse.*/sse"
-                              lyric
-                              "")
-                             "[Verse 1"))))
+(defun genius-mapper (xs)
+  (values
+   (cl-ppcre:regex-replace "(?s).*Verse 1"
+                           (cl-ppcre:regex-replace
+                            "(?s)/sse.*/sse"
+                            (reduce (lambda (x acc) (concatenate 'string x acc)) xs)
+                            "")
+                           "[Verse 1")))
 
-(defun create-wikia-lyric-url (artist song)
+(defun get-genius (artist song)
+  (get-lyric-with-dex artist
+                      song
+                      #'genius-url-creator
+                      "div.lyrics"
+                      #'genius-mapper))
+
+(defun wikialyric-url-creator (artist song)
   (flet ((space->title-case-with-_ (x)
            (space-> (cl-strings:title-case x) "_")))
     (format nil
@@ -71,23 +96,30 @@
             (space->title-case-with-_ artist)
             (space->title-case-with-_ song))))
 
-(defun get-wikia-lyric (artist song)
-  (let* ((request (dex:get (create-wikia-lyric-url artist song)))
-         (parsed-content (plump:parse request))
-         (lyric-vec (lquery:$  parsed-content "div.lyricbox" (text)))
-         (lyric (reduce (lambda (x acc)  (concatenate 'string x acc))  lyric-vec))) 
-    (values lyric)))
+(defun wikialyric-mapper (xs)
+  (values
+   (reduce (lambda (x acc) (concatenate 'string x acc)) xs)))
 
-(defun create-songlyrics-url (artist song)
+(defun get-wikialyric (artist song)
+  (get-lyric-with-dex artist
+                      song
+                      #'wikialyric-url-creator
+                      "div.lyricbox"
+                      #'wikialyric-mapper))
+
+(defun songlyrics-url-creator (artist song)
   (format nil
           "http://www.songlyrics.com/~a/~a-lyrics/"
           (space->- artist)
           (space->- song)))
 
-(defun get-songlyrics (artist song)
-  (let* ((request (dex:get (create-songlyrics-url artist song)))
-         (parsed-content (plump:parse request))
-         (lyric-vec (lquery:$  parsed-content "#songLyricsDiv" (text)))
-         (lyric (reduce (lambda (x acc)  (concatenate 'string x acc))  lyric-vec))) 
-    (values lyric)))
+(defun songlyrics-mapper (xs)
+  (values
+   (reduce (lambda (x acc) (concatenate 'string x acc)) xs)))
 
+(defun get-songlyrics (artist song)
+  (get-lyric-with-dex artist
+                      song
+                      #'songlyrics-url-creator
+                      "#songLyricsDiv"
+                      #'songlyrics-mapper))
